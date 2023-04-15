5cbda41911aad59349bd176ea826adbc1037cff7
Владислав Черепков <trinitikvintessentsia@yandex.ru>
2023-04-15 20:10:38 +0300

    add working version without classes

--- test_module.py
+++ test_module.py
@@ -11,3 +11,7 @@ def test_program():
     for filename in files_list:
         assert filename in dir_files, f'Файл `{filename}` не найден в корне репозитория'
 
+
+def ooi(a:str, b:str) -> bool:
+    return a == b
+
